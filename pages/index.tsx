94d40b8164a04dfc20d28a70ffd20d5dc907c67e
Vishal Pawar <vishalpawar425@gmail.com>
2023-11-06 21:28:34 +0530

    project init

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <h1 className='text-2xl'>test me</h1>
+  )
+}
